bd2cd9bb1f8c369f5364f66c3ab579a62e383fa8
Masaaki Hoshi <hoshi@hyakuren.org>
2016-03-06 17:10:46 +0900

    Add ArticlesController#index,create and view. (MVP1)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,25 @@
 class ArticlesController < ApplicationController
+  before_action :build_article, only: [:index, :create]
+
+  def index
+    @articles = Article.all.order(created_at: :desc)
+  end
+
+  def create
+    @article.save!
+    redirect_to root_path, notice: 'Thanks a lot :D'
+  rescue
+    flash.now[:errors] = 'Problem has occured... Try again.'
+    render :index
+  end
+
+  private
+
+  def build_article
+    @article = Article.new(article_params)
+  end
+
+  def article_params
+    params.require(:article).permit(:content) if params[:article]
+  end
 end
